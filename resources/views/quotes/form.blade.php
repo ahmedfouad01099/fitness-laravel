51f66b8e6e081ee35be54080917647e0900e11cf
Ahmed Mohamed Fouad <63275575+ahmedfouad01099@users.noreply.github.com>
2025-10-28 17:15:05 +0300

    finish adding forms for quotes

---
+++ resources/views/quotes/form.blade.php
@@ -0,0 +1,61 @@
+@push('scripts')
+@endpush
+<x-app-layout :assets="$assets ?? []">
+    <div>
+        <?php $id = $id ?? null;?>
+        @if(isset($id))
+            <form action="{{ route('quotes.update', $id) }}" method="POST">
+                @method('PATCH')
+                @csrf
+        @else
+                <form action="{{ route('quotes.store') }}" method="POST">
+                    @csrf
+            @endif
+                <div class="row">
+                    <div class="col-lg-12">
+                        <div class="card">
+                            <div class="card-header d-flex justify-content-between">
+                                <div class="header-title">
+                                    <h4 class="card-title">{{ $pageTitle }}</h4>
+                                </div>
+                                <div class="card-action">
+                                    <a href="{{ route('quotes.index') }} " class="btn btn-sm btn-primary"
+                                        role="button">{{ __('message.back') }}</a>
+                                </div>
+                            </div>
+
+                            <div class="card-body">
+                                <div class="row">
+                                    <div class="form-group col-md-6">
+                                        <label for="title" class="form-control-label">{{ __('message.title') }} <span
+                                                class="text-danger">*</span></label>
+                                        <input type="text" name="title" id="title"
+                                            value="{{ old('title', isset($data) ? $data->title : '') }}"
+                                            placeholder="{{ __('message.title') }}" class="form-control" required>
+                                    </div>
+                                    <div class="form-group col-md-6">
+                                        <label for="date" class="form-control-label">{{ __('message.date') }} <span
+                                                class="text-danger">*</span></label>
+                                        <input type="text" name="date" id="date"
+                                            value="{{ old('date', isset($data) ? $data->date : '') }}"
+                                            placeholder="{{ __('message.date') }}" class="datepicker form-control"
+                                            required>
+                                    </div>
+                                    <div class="form-group col-md-12">
+                                        <label for="message" class="form-control-label">{{ __('message.message') }}
+                                            <span class="text-danger">*</span></label>
+                                        <textarea name="message" id="message" class="form-control textarea" rows="3"
+                                            required
+                                            placeholder="{{ __('message.message') }}">{{ old('message', isset($data) ? $data->message : '') }}</textarea>
+                                    </div>
+                                </div>
+                                <hr>
+                                <button type="submit"
+                                    class="btn btn-md btn-primary float-end">{{ __('message.save') }}</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </form>
+    </div>
+</x-app-layout>
